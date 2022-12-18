5e6918ae0b5490bc87215bee1fa1d3f6f3724613
SnxwyNotFound <88547407+SnxwyNotFound@users.noreply.github.com>
2022-12-18 17:12:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+left_wrist=0;
+right_wrist=0;
+difference=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(450,400);
+    canvas=createCanvas(450,400);
+    canvas.position(650,250);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+function draw(){
+    background("#667e9b");
+    fill("#daf3f7");    
+    fill("#ebf2f2");
+    text("Aarabhi", 50,50);
+    textSize(difference);
+}
+function modelLoaded(){
+    console.log("PoseNet is inisialized !!")
+}
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+        left_wrist=results[0].pose.leftWrist.x;
+        right_wrist=results[0].pose.rightWrist.x;
+        console.log("Left Wrist = "+left_wrist+"Right Wrist = "+right_wrist);
+        difference=floor(left_wrist-right_wrist);
+        console.log(left_wrist,right_wrist,difference)
+    }
+}
